51b69a43d892892d04483476ddf0dad6c2d8b61c
Oliver Viljamaa <oliver.viljamaa@gmail.com>
2017-11-03 17:17:04 +0000

    Show icons on demo page, add simple styles

--- src/demo/build.js
+++ src/demo/build.js
@@ -7,15 +7,40 @@ const { getIconNames } = require('../utils/icon');
 
 
 const TEMPLATE_PATH = path.join(__dirname, 'views', 'index.ejs');
+const DEMO_STYLES_PATH = path.join(__dirname, 'styles.css');
+const ICON_STYLES_PATH = path.join(__dirname, '..', '..', 'dist', 'icons.min.css');
+const SPRITE_PATH = path.join(__dirname, '..', '..', 'dist', 'sprite.svg');
 const DEMO_PATH = path.join(__dirname, '..', '..', 'demo');
 
 function build() {
+  const demoStyles = getDemoStyles();
+  const iconStyles = getIconStyles();
+  const sprite = getSprite();
   const names = getIconNames();
 
-  ejs.renderFile(TEMPLATE_PATH, { names }, {}, (err, str) => {
+  const data = {
+    demoStyles,
+    iconStyles,
+    sprite,
+    names,
+  };
+
+  ejs.renderFile(TEMPLATE_PATH, data, {}, (err, str) => {
     mkdirp.sync(DEMO_PATH);
     fs.writeFileSync(path.join(DEMO_PATH, 'index.html'), str);
   });
+}
+
+function getDemoStyles() {
+  return fs.readFileSync(DEMO_STYLES_PATH, 'utf-8');
+}
+
+function getIconStyles() {
+  return fs.readFileSync(ICON_STYLES_PATH, 'utf-8');
+}
+
+function getSprite() {
+  return fs.readFileSync(SPRITE_PATH, 'utf-8');
 }
 
 module.exports = build;
