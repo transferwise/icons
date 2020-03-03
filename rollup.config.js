ee5970290ed7e2f38885a82f9879e749520eeb02
Domi Horvath <domonkos.horvath@transferwise.com>
2020-03-03 17:55:35 +0100

    chore: tweak rollup config, add plugins

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,19 @@
 import typescript from 'rollup-plugin-typescript2';
+import analyze from 'rollup-plugin-analyzer';
 import pkg from './package.json';
+import { terser } from "rollup-plugin-terser";
 
 export default {
   input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
+      name: pkg.name,
       format: 'umd',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+      }
     },
     {
       file: pkg.module,
@@ -15,11 +22,14 @@ export default {
   ],
   external: [
     ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.devDependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
     typescript({
       typescript: require('typescript'),
     }),
+    terser(),
+    analyze({ summaryOnly: true }),
   ],
 }
