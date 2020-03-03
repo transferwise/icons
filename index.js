be03b28d7a8744d4432e1d5beaf8e0480bd6e576
Domi Horvath <domonkos.horvath@transferwise.com>
2020-03-03 17:55:14 +0100

    feat: delete old index.js file

--- index.js
+++
@@ -1,7 +0,0 @@
-const svgIconSprite = require('./dist/svg-icon-sprite');
-const svgIconSpriteVersion = require('./dist/svg-icon-sprite-version');
-
-module.exports = {
-  svgIconSprite,
-  svgIconSpriteVersion,
-};
