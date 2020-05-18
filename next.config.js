cc0c012615db5a10061781471e6622166e3d9c81
Domi Horvath <domonkos.horvath@transferwise.com>
2020-05-18 19:08:02 +0200

    fix: add assetPrefix to next.js config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const package = require('./package.json');
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
 
 module.exports = {
+  assetPrefix: isProd ? '/icons' : '',
   publicRuntimeConfig: {
     libVersion: `v${package.version}`,
   },
